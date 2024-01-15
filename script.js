a430df7ecc614444fbc735a7a006d708a50cc50b
Mrimperfect7 <110906985+Mrimperfect7@users.noreply.github.com>
2024-01-16 00:20:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-document.getElementById('menu-toggle').addEventListener('click', function() {
-    const drawer = document.getElementById('drawer');
-    drawer.classList.toggle('closed');
+const drawers = document.querySelectorAll('.drawer');
+
+drawers.forEach((drawer, index) => {
+    drawer.addEventListener('click', () => {
+        toggleDrawers(index);
+    });
 });
+
+function toggleDrawers(selectedIndex) {
+    drawers.forEach((drawer, index) => {
+        if (index === selectedIndex) {
+            drawer.classList.toggle('active');
+        } else {
+            drawer.classList.remove('active');
+        }
+    });
+}
